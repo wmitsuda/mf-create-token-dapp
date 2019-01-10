8529da1133429a77ac9abbe3d624e3e7cdba9da8
Doug <douglaswleonard+git@gmail.com>
2019-01-10 17:54:21 +0000

    test if sdk is working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainframeSDK from '@mainframe/sdk'
 
 class App extends Component {
+
+  sdk = new MainframeSDK()
+
+  state = {
+    sdkWorking: false
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    if (this.sdk.blockchain.getWeb3Provider() !== null) {
+      this.setState({sdkWorking: true})
+    }
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
+          <h1>sdkWorking: { this.state.sdkWorking ? "true" : "false" }</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
