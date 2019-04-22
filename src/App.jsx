b28ae96bf2516e1e1263174a2c6d470acf1f6b15
Willian Mitsuda <wmitsuda@gmail.com>
2019-04-22 19:27:39 -0300

    Implemented all remaining validations

--- src/App.jsx
+++ src/App.jsx
@@ -21,13 +21,6 @@ const initialValues = {
   initialOwner: "0xe3f0D0ECfD7F655F322A05d15C996748Ad945561"
 };
 
-const validationSchema = Yup.object().shape({
-  tokenName: Yup.string().required("Value is required"),
-  tokenSymbol: Yup.string().required("Value is required"),
-  initialSupply: Yup.string().required("Value is required"),
-  initialOwner: Yup.string().required("Value is required")
-});
-
 const StyledDiv = styled.div`
   margin: 20px;
   padding: 20px;
@@ -38,6 +31,37 @@ const web3Options = {
 };
 
 const web3 = new Web3(sdk.ethereum.web3Provider, null, web3Options);
+
+const validationSchema = Yup.object().shape({
+  tokenName: Yup.string()
+    .required("Value is required")
+    .min(5, "Token name must have a minimum of 5 characters")
+    .max(30, "Token name must have a maximum of 30 characters")
+    .matches(
+      /^\s*\w+[\w\s]*$/,
+      "Token name must have at least one alphanumeric character"
+    ),
+  tokenSymbol: Yup.string()
+    .required("Value is required")
+    .min(1, "Token symbol must have a minimum of 1 character")
+    .max(20, "Token symbol must have a maximum of 20 characters")
+    .matches(
+      /^[A-Z][A-Z\d]*$/,
+      "Token symbol must start with a letter followed by letters or numbers"
+    ),
+  initialSupply: Yup.number()
+    .typeError("Value must be an integer number")
+    .integer("Value must be an integer number")
+    .required("Value is required")
+    .min(1, "Value must be at least 1")
+    .max(1000000000, "Value must be less than 1 billion"),
+  initialOwner: Yup.string()
+    .trim()
+    .required("Value is required")
+    .test("isAddress", "Value is not an ETH address", value => {
+      return web3.utils.isAddress(value);
+    })
+});
 
 const App = () => {
   const [transactionHash, setTransactionHash] = useState();
@@ -59,15 +83,16 @@ const App = () => {
     try {
       setStep(0);
 
+      const decimals = 18;
       const contract = await erc20
         .deploy({
           data: StandardERC20Token.bytecode,
           arguments: [
             values.tokenName,
             values.tokenSymbol,
-            18,
-            values.initialOwner,
-            values.initialSupply.toString() + "0".repeat(18)
+            decimals,
+            values.initialOwner.trim(),
+            parseInt(values.initialSupply).toString() + "0".repeat(decimals)
           ]
         })
         .send({ from: defaultAccount })
